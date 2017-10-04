817e16135a2e4e0234dde820f3396ecdb9bb389c
Kit Masaracchia <kit.masaracchia@gmail.com>
2017-10-03 19:46:11 -0600

    Axios Todo: continued to refactor with jQuery, attempted to add event.target functionality

--- exercises/axios-refactored/todo.js
+++ exercises/axios-refactored/todo.js
@@ -1,51 +1,45 @@
 /////// DISPLAY FUNCTIONS \\\\\\\\
 const getList = () => {
     axios.get("https://api.vschool.io/kit/todo").then((response) => {
-        const todoList = response.data;
-        todoList.forEach((currentTodo) => {
-            const editId = currentTodo._id;
-            const thisTitle = currentTodo.title;
-            const thisDescription = currentTodo.description;
-            const thisPrice = parseInt(currentTodo.price);
+        const theTodos = response.data;
+        $("#todo-list").html("");
+        theTodos.forEach(function (todo, i) {
+            let editId = response.data[i]._id;
 
             $("#todo-list").append(`
-        <div id=${editId} class="grab-edit-id">
-            <div class="each-todo-container">
-                <i id="checkbox" class="fa fa-circle-o fa-lg checkbox"></i>
-                <input id="title" class="each-todo-title" value="${thisTitle}" disabled>
-            </div>
-            <div id="description" class="description-container">
-                <div class="subtitles">
-                    <p class="inline">Description</p>
-                    <div class="inline price-container">
-                        <p class="inline">PRICE:</p>
-                        <input id="price" type="number" class="inline price-text" value="${thisPrice}" disabled>
-                    </div>
+                <form id=${editId} class="each-todo-container">
+                    <i name="checkbox" class="fa fa-circle-o fa-lg checkbox"></i>
+                    <input name="title" class="each-todo-title" value="${todo.title}" disabled>
                 </div>
-                <textarea id="description-text" class="description-text" disabled>${thisDescription}</textarea>
-            </div>
-            <div class="edit-container">
-                <p id="edit" class="edit">EDIT</p>
-                <p id="delete" class="delete" onclick="deleteItem('${editId}')">DELETE</p>
-            </div>
-        </div>
+                <div name="description" class="description-container">
+                    <div class="subtitles">
+                        <p class="inline">Description</p>
+                        <div class="inline price-container">
+                            <p class="inline">PRICE:</p>
+                            <input name="price" type="number" class="inline price-text" value=${todo.price} disabled>
+                        </div>
+                    </div>
+                    <textarea name="description-text" class="description-text" disabled>${todo.description}</textarea>
+                </div>
+                <div class="edit-container">
+                    <button name="edit" type="button" class="edit">EDIT</button>
+                    <button name="delete" type="button" class="delete">DELETE</button>
+                </form>
             `);
         });
+        $("#delete").click(deleteItem);
     });
 };
 
-
 ////// EVENT LISTENERS \\\\\\\
-
+/// ADD \\\
 $("#add-button").click(function () {
-    const newTitle = $("#new-title").val();
-    const newPrice = $("#new-price").val();
-    const newDescription = $("#new-description").val();
+    const addTodo = $("#add-item")["0"];
 
     const newTodo = {
-        title: newTitle,
-        price: newPrice,
-        description: newDescription
+        title: addTodo.newTitle.value,
+        price: addTodo.newPrice.value,
+        description: addTodo.newDescription.value
     }
 
     axios.post("https://api.vschool.io/kit/todo", newTodo).then((response) => {
@@ -55,36 +49,44 @@ $("#add-button").click(function () {
 });
 
 
-///// DELETE \\\\\
-// ADDED AS ONCLICK ON EACH #DELETE \\
-const deleteItem = (id) => {
-    axios.delete(`https://api.vschool.io/kit/todo/${id}`).then((response) => {
-        $(`#${id}`).remove();
-    })
+/// DELETE \\\
+//const deleteItem = (id) => {
+//    axios.delete(`https://api.vschool.io/kit/todo/${id}`).then((response) => {
+//        $(`#${id}`).remove();
+//    })
+//}
+
+const deleteItem = (event) => {
+    const target = event.target;
+//    const deleteId = $("#delete").closest("form").prop("id");
+    console.log(target);
 }
 
-
-///// EDIT \\\\\\
-// ADDED AS ONCLICK ON EACH #EDIT \\
-const editItem = (id) => {
-    const editedTitle = $("#new-title").val();
-    const editedPrice = $("#new-price").val();
-    const editedDescription = $("#new-description").val();
-
-    const newTodo = {
-        title: newTitle,
-        price: newPrice,
-        description: newDescription
-    }
-    axios.put(`https://api.vschool.io/kit/todo/${id}`).then((response) => {
-
-    });
+const working = () => {
+    console.log("working");
 }
+
+/// EDIT \\\\
+//const editItem = (id) => {
+//    if 
+//    
+//    const editedTitle = $("#new-title").val();
+//    const editedPrice = $("#new-price").val();
+//    const editedDescription = $("#new-description").val();
+//
+//    const newTodo = {
+//        title: newTitle,
+//        price: newPrice,
+//        description: newDescription
+//    }
+//    axios.put(`https://api.vschool.io/kit/todo/${id}`).then((response) => {
+//
+//    });
+//}
 
 
 $(document).ready(() => {
     getList();
     //    document.getElementById("header").style.background = "url('https://source.unsplash.com/daily?mountain') no-repeat center";
-//    document.getElementById("header").style.backgroundSize = "cover";
+    //    document.getElementById("header").style.backgroundSize = "cover";
 });
-
