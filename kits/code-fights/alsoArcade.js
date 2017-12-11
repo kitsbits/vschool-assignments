4e4557a0f23525ec73393eaaa9b3e52eb03c9247
Kit Masaracchia <kit.masaracchia@gmail.com>
2017-12-11 15:06:28 -0500

    update code fights

--- kits/code-fights/alsoArcade.js
+++ kits/code-fights/alsoArcade.js
@@ -47,41 +47,122 @@
 
 ////
 
+
 const s1 = "aabcc";
 const s2 = "adcaa";
-// function commonCharacterCount(s1, s2) {
-//     const one = s1.split("").reduce((allLetters, eachLetter) => {
-//         console.log(allLetters)
-//         if (eachLetter in allLetters) {
-//             allLetters[eachLetter]++;
-//         } else {
-//             allLetters[eachLetter] = 1;
-//         }
-//     }, {});
-//     const two = s2.split("").reduce((allLetters, eachLetter) => {
-//         if (eachLetter in allLetters) {
-//             allLetters[eachLetter]++;
-//         } else {
-//             allLetters[eachLetter] = 1;
-//         }
-//     }, {});
+function commonCharacterCount(s1, s2) {
+    const one = s1.split("").reduce((allLetters, eachLetter) => {
+        console.log(allLetters)
+        if (eachLetter in allLetters) {
+            allLetters[eachLetter]++;
+        } else {
+            allLetters[eachLetter] = 1;
+        }
+        return allLetters;
+    }, {});
+    const two = s2.split("").reduce((allLetters, eachLetter) => {
+        if (eachLetter in allLetters) {
+            allLetters[eachLetter]++;
+        } else {
+            allLetters[eachLetter] = 1;
+        }
+        return allLetters;
+    }, {});
+
+    let letterCount = 0;
+    for (letters in one) {
+        if (letters in two) {
+            letterCount += Math.min(one[letters], two[letters]);
+        }
+    }
+
+    return letterCount;
+
+}
+
+// console.log(commonCharacterCount(s1, s2));
+
+// const s1 = "aabcc";
+// const s2 = "adcaa";
 //
-//     let letterCount = 0;
-//     for (letters in one) {
-//         if (letters in two) {
-//             letterCount += Math.min(one[letters], two[letters]);
+// function commonCharacterCount(s1, s2) {
+//     let testArray = [];
+//     for (let i = 0; i < s1.length; i++) {
+//         const regex = new RegExp(s1[i], "g");
+//         const match = s2.match(regex);
+//         if (match !== null) {
+//             testArray.push(match);
 //         }
 //     }
+//     return testArray.length;
+// }
+
+
+    // let regex = new RegExp(`[${s1}]`, "g");
+    // const test2 = s2.match(regex);
+    //
+    // let regex2 = new RegExp(`[${s2}]`, "g");
+    // const test1 = s1.match(regex2);
+    //
+    // return [...test1, ...test2];
+
+// const n = 1203;
+// const m = 134888;
 //
-//     return letterCount;
+// function isLucky(n) {
+//     let toString = n.toString();
+//     const half = toString.split("").length / 2;
+//     let firstHalf = toString.substr(0, half);
+//     let secondHalf = toString.substr(half);
+//
+//     let firstSum = 0;
+//     let secondSum = 0;
+//
+//     for (let i in firstHalf) {
+//         let numI = Number(firstHalf[i]);
+//         firstSum += numI;
+//     }
+//
+//     for (let i in secondHalf) {
+//         let numI = Number(secondHalf[i]);
+//         secondSum += numI;
+//     }
+//
+//     if (firstSum === secondSum) {
+//         return true;
+//     }
+//
+//     return false;
 //
 // }
 //
-// console.log(commonCharacterCount(s1, s2));
+// console.log(isLucky(m));
 
-const s1 = "aabcc";
-const s2 = "adcaa";
+// const a = [-1, 150, 190, 170, -1, -1, 160, 180];
+//
+// function sortByHeight(a) {
+//     const indices = [];
+//     for (let i in a) {
+//         if (a[i] === -1) {
+//             indices.push(Number(i));
+//         }
+//     }
+//     const sortedTrees = a.filter(number => number !== -1).sort((a, b) => a - b);
+//
+//     for (let i in indices) {
+//         sortedTrees.splice(indices[i], 0, -1);
+//     }
+//
+//     return sortedTrees;
+// }
+//
+// console.log(sortByHeight(a));
 
-function commonCharacterCount(s1, s2) {
+const s = "a(bc)de";
+const t = "co(de(fight)s)";
+
+function reverseParentheses(s) {
     
 }
+
+console.log(reverseParentheses(s))
